acb41c3b018a30ea1efc296738ca1c6531801688
adrianmulyawan <adrianmulyawan666@gmail.com>
2023-11-10 11:39:43 +0700

    import navbar to homepage

---
+++ src/pages/home.page.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import HeaderPart from '../parts/header.part';
+
+const HomePage = () => {
+  return (
+    <>
+      <HeaderPart />
+      <h1>Ini Home page</h1>
+    </>
+  );
+}
+
+export default HomePage;
