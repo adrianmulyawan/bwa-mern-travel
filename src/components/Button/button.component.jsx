25ede447fd5922fadf8b4c1fd3a2ac77e0cad966
adrianmulyawan <adrianmulyawan666@gmail.com>
2023-11-14 17:30:51 +0700

    update button component

--- src/components/Button/button.component.jsx
+++ src/components/Button/button.component.jsx
@@ -5,89 +5,67 @@ import { Link } from 'react-router-dom';
 
 const ButtonComponent = (props) => {
   const className = [props.className];
-
-  if (props.isPrimary) {
-    className.push("btn-primary")
-  }
-  if (props.isLarge) {
-    className.push("btn-lg")
-  }
-  if (props.isSmall) {
-    className.push('btn-sm');
-  }
-  if (props.isBlock) {
-    className.push('btn-block');
-  }
-  if (props.hasShadow) {
-    className.push('btn-shadow')
-  }
+  if (props.isPrimary) className.push("btn-primary");
+  if (props.isLight) className.push("btn-light");
+  if (props.isLarge) className.push("btn-lg");
+  if (props.isSmall) className.push("btn-sm");
+  if (props.isBlock) className.push("btn-block");
+  if (props.hasShadow) className.push("btn-shadow");
 
   const onClick = () => {
-    if (props.onClick) {
-      props.onClick()
-    }
-  }
+    if (props.onClick) props.onClick();
+  };
 
   if (props.isDisabled || props.isLoading) {
-    if (props.isDisabled) {
-      className.push("disabled");
-    }
+    if (props.isDisabled) className.push("disabled");
     return (
-      <span
-        className={ className.join(" ") }
-        style={ props.style }
-      >
-        { 
-          props.isLoading ? (<>
-            <span className='spinner-border spinner-border-sm mx-5'></span>
-            <span className='sr-only'>Loading...</span>
-          </>) : (
-            props.children
-          )
-        }
+      <span className={className.join(" ")} style={props.style}>
+        {props.isLoading ? (
+          <>
+            <span className="spinner-border spinner-border-sm mx-5"></span>
+            <span className="sr-only">Loading...</span>
+          </>
+        ) : (
+          props.children
+        )}
       </span>
     );
   }
 
-  // > Cek apakah yang dikirim itu link
-  if (props.type === 'link') {
-    // > Link external?
+  if (props.type === "link") {
     if (props.isExternal) {
       return (
-        <a 
-          href={ props.href } 
-          className={ className.join(' ') } 
-          style={ props.style } 
-          target={ props.target === "_blank" ? "_blank" : undefined } 
-          rel={ props.target === "_blank" ? "noopener noreferrer" : undefined }
+        <a
+          href={props.href}
+          className={className.join(" ")}
+          style={props.style}
+          target={props.target === "_blank" ? "_blank" : undefined}
+          rel={props.target === "_blank" ? "noopener noreferrer" : undefined}
         >
-          { props.children }
+          {props.children}
         </a>
       );
-    } 
-    // > Link internal atau route didalam aplikasi
-    else {
+    } else {
       return (
         <Link
-          to={ props.href } 
-          className={ className.join(' ') } 
-          style={ props.style } 
-          onClick={ onClick }
+          to={props.href}
+          className={className.join(" ")}
+          style={props.style}
+          onClick={onClick}
         >
-          { props.children }
+          {props.children}
         </Link>
       );
     }
   }
 
-  // > Ini untuk type === button
   return (
-    <button 
-      className={ className.join('') }
-      style={ props.style }
-      onClick={ onClick }
+    <button
+      className={className.join(" ")}
+      style={props.style}
+      onClick={onClick}
     >
-      { props.children }
+      {props.children}
     </button>
   );
 }
